b8f31acc1bad20dc3178ae843afa74edc7417db3
SaintEfim <saintefim2@gmai.com>
2024-10-03 21:27:37 +0300

    fixed error middleware

--- internal/middleware/logging_middleware.go
+++ internal/middleware/logging_middleware.go
@@ -13,19 +13,21 @@ import (
 func LoggingMiddleware(logger *zap.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		startTime := time.Now()
-		requestBody, err := readRequestBody(c.Request.Body)
-
-		if err != nil {
-			logger.Error("Failed to read request body", zap.Error(err))
-		}
 
 		logFields := []zap.Field{
 			zap.String("client_ip", c.ClientIP()),
 			zap.String("user_agent", c.Request.UserAgent()),
 			zap.String("header", fmt.Sprintf("%v", c.Request.Header)),
-			zap.String("request_body", requestBody),
 			zap.String("query_parameters", c.Request.URL.Query().Encode()),
 			zap.String("size_request", fmt.Sprintf("%d", c.Writer.Size())),
+		}
+
+		requestBody, err := readRequestBody(c.Request.Body)
+
+		if err != nil {
+			logger.Error("Failed to read request body", zap.Error(err))
+		} else {
+			logFields = append(logFields, zap.String("request_body", requestBody))
 		}
 
 		logger.Info("Incoming request",
